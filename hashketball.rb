d92b2111a15e69241ace8109b2988284cbda4767
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-09 20:29:41 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -24,7 +24,7 @@ end
 def team_colors(team)
   $data.map do |key, value|
     if key[:team_name] == team
-      value[:colors].flatten\
+      value[:colors].flatten
       binding.pry
     end
   end
